4c92f8764f7c1201fdbb5fb4256284af37c906af
Robert Moelker <rmoelker@gmail.com>
2025-04-04 14:50:53 +0200

    feat: setup route with advanced filtering system

--- src/graphql.ts
+++ src/graphql.ts
@@ -15,29 +15,14 @@ export const typeDefs = `#graphql
     createdAt: Int
   }
 
+  input UserFilter {
+    emailAddress: String
+    lastName: String
+    active: Boolean
+  }
+
   type Query {
-    users: [User]
-    products(id: ID!): [Product]
+    users(filters: UserFilter): [User]
+    product(id: String!): Product
   }
 `
-
-// Setup a resolver - so that ApolloServer knows which list to return on what certain query
-export const resolvers = {
-  Query: {
-    users: () => users,
-    // products: (userId: ID!): [Product 
-    // TODO: Investigate how the nested structure will work
-    // e.g. fetch all the orders related to the `users` could be connected via 
-    // [{ 
-    // user_id: xyz, 
-    // emailAddress: 'r.moelker@labdigital.nl', 
-    // orders: [{ order_id: 'xyz', 
-    //   products: [{ 
-    //     name: 'product-name-here', price: 500.00 
-    //   }, { 
-    //     name: 'second-product-here', price: 585.00 
-    //   }]
-    // }]
-    // orders: () => orders,
-  },
-}
--- src/index.ts
+++ src/index.ts
@@ -2,31 +2,74 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import shuffle from 'lodash/shuffle.js'
 import { getProducts, getUsers } from './faker.js'
-import { typeDefs, resolvers } from './graphql.js'
+import { typeDefs } from './graphql.js'
+import { GraphQLError } from 'graphql'
+import 'dotenv/config'
 
 const products = getProducts(10)
 const users = getUsers(100)
 
 // Inject products into the user
 users.forEach(user => {
-  for (let p = 0; p < Math.floor(Math.random() * 10); p++) {
+  for (let i = 0; i < Math.floor(Math.random() * 10); i++) {
     const userProducts = shuffle(products)
-    if (!user.products) {
-      user.products = []
-    }
 
-    user.products.push(userProducts[p])
+    user.products = user.products ?? []
+    user.products.push(userProducts[i])
   }
 })
 
+// Setup the required resolvers
+const resolvers = {
+  Query: {
+    users: (_, { filters }) => users.filter(user => {
+      console.log('user', user)
+      for (const key in filters) {
+        if (filters[key] !== user[key]) {
+          return false
+        }
+      }
+
+      return true
+    }),
+    product: (_, args, context) => {
+      console.log('context', context)
+
+      return products.find(product => product.id === args.id)
+    }
+  }
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
 
-const { url } = await startStandaloneServer(server, { 
-  listen: { port: 7589 }
+const isValid = (username: string, password: string) => {
+  return username === process.env.USERNAME && password === process.env.PASSWORD
+}
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: parseInt(process.env.PORT || '7589') },
+  context: async ({ req }) => {
+    const [,user] = req.headers.authorization.split(' ')
+    const [username, password] = atob(user).split(':')
+
+    if (!user || !isValid(username, password)) {
+      throw new GraphQLError('You\'re not authenticated', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+          http: { status: 401 }
+        }
+      })
+    }
+
+    return {
+      user: {
+        name: username
+      } 
+    }
+  }
 })
 
-console.log(`Server launched at: ${url}`)
+console.log(`🚀 Server launched at: ${url}`)
